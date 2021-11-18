8862d4f2c158fb5708a82331901e035ef7502058
AlekseyLevko <AlekseyLevko@gmail.com>
2021-11-18 10:16:02 +0300

    feat: add jest; add test and test:coverage scripts to project

---
+++ test/checkArgumentsForDuplicates.test.js
@@ -0,0 +1,5 @@
+const checkArgumentsForDuplicates = require("../checkArgumentsForDuplicates");
+
+test("User passes the same cli argument twice", () => {
+  //expect(checkArgumentsForDuplicates().toBe(5));
+});
